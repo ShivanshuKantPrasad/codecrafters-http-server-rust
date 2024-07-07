55376e6eb6b39846d5c75e8ab1c1f729fb2df713
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-07-07 13:43:51 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,7 +10,22 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+
+                let buf_reader = BufReader::new(&mut stream);
+                let http_request: Vec<_> = buf_reader
+                    .lines()
+                    .map(|result| result.unwrap())
+                    .take_while(|line| !line.is_empty())
+                    .collect();
+                let request = http_request[0].clone();
+                dbg!("{request}");
+                let filepath = request.split_whitespace().nth(1).unwrap();
+                dbg!("{filepath}");
+                if filepath == "/" {
+                    let _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+                } else {
+                    let _ = stream.write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
